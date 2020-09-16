836eecac9f1c7ecb4807fe27896c42987a7e817e
Jerem-67 <jeremy67230@gmail.com>
2020-09-16 15:32:35 +0200

    data table ok

---
+++ app/Http/Controllers/ReviewsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reviews;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class ReviewsController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $reviews =  Reviews::get();
+        return view('reviews.index', [
+            'reviews' => $reviews
+        ]);
+    }
+
+    public function create()
+    {
+        return view('reviews.new');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'bail|required|email',
+            'content' => 'bail|required|max:500'
+        ]);
+
+        $review = new Reviews;
+        $review->email = $request->email;
+        $review->name = $request->name;
+        $review->rate = $request->rate;
+        $review->content = $request->content;
+        $review->save();
+        return redirect()->route('reviews.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ReviewsController::class, 'index'])->name('reviews.index');
+Route::get('/new_review', [ReviewsController::class, 'create'])->name('reviews.create');
+Route::post('/new_review', [ReviewsController::class, 'store'])->name('reviews.store');
